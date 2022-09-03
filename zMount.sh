d26be3b8c15533043de0b3eda8791e022279b3b0
scawp <rob@scawp.com>
2022-09-03 16:23:25 +0200

    Complete rewrite to simplify the process

--- zMount.sh
+++ zMount.sh
@@ -2,235 +2,80 @@
 #Steam Deck Mount External Drive by scawp
 #License: DBAD: https://github.com/scawp/Steam-Deck.Mount-External-Drive/blob/main/LICENSE.md
 #Source: https://github.com/scawp/Steam-Deck.Mount-External-Drive
+# Use at own Risk!
 
 tmp_dir="$(dirname "$(realpath "$0")")/tmp"
 
 external_drive_list="$tmp_dir/external_drive_list.txt"
-external_drive_info="$tmp_dir/external_drive_info.txt"
-external_part_list="$tmp_dir/external_partition_list.txt"
 
-mount_path="/run/media/deck/"
+function list_gui () {
+  #IFS=$'\t';
+  selected_device=$(zenity --list --title="Select Drive to (un)Mount" \
+    --width=1000 --height=720 --print-column=ALL   --separator="\t" \
+    --ok-label "(Un)Mount" --extra-button "Refresh" \
+    --column="Path" --column="Size" --column="UUID" \
+    --column="Mount Point" \
+    $(cat "$1" | sed -e 's/$/\t/'))
+  ret_value="$?"
+  #unset IFS;
+}
 
-zenity_timeout=3 #seconds
+function confirm_gui () {
+  zenity --question --width=400 \
+    --text="Do you want to $1 device $2 with uuid of $3?"
 
+  if [ "$?" = 1 ]; then
+    exit 1;
+  fi
+}
 
-#create temp dir if missing
-if [ ! -d "$tmp_dir" ]; then
-  echo "creating tmp dir"
-  mkdir "$tmp_dir"
-fi
-
-#TODO this is to fore kdesu dialog to use sudo, maybe theres a better way?
-if [ ! -f ~/.config/kdesurc ];then
-  touch ~/.config/kdesurc
-  echo "[super-user-command]" > ~/.config/kdesurc
-  echo "super-user-command=sudo" >> ~/.config/kdesurc
-fi
-
-#purge temp files
-true > "$external_drive_list"
-true > "$external_drive_info"
-true > "$external_part_list"
-
-show_sd_card=0
-if [ "$1" = "sd" ]; then
-  show_sd_card=1
-fi
+function get_drive_list () {
+  lsblk -nlo HOTPLUG,PATH,SIZE,UUID,MOUNTPOINT | grep -i '^\s*1' | awk  '$4!=""' | awk '{ if ( $5!="") print $2"\t"$3"\t"$4"\t"$5;else print $2"\t"$3"\t"$4"\tUnmounted"}' > "$external_drive_list"
+}
 
 function do_mount () {
-  if [ ! -n "$2" ] ; then
-    echo "No Drive Selected"
-    exit 1;
-  fi
-
-  if [ "$1" = "Unmount" ] ; then
-    kdesu -c "umount \"$2\""
+  if [ "$1" = "mount" ]; then
+    ret_value="$(udisksctl mount -b "$2")"
   else
-    if grep -i '^UUID='$3 /etc/fstab; then
-      #if theres an entry in fstab use those values (by default)
-      kdesu -c "mount $mount_path$4"
+    ret_value="$(udisksctl unmount -b "$2")"
+  fi
+}
+
+function main () {
+  get_drive_list
+  list_gui "$external_drive_list"
+  echo "$ret_value"
+  mount_point="$(echo "$selected_device" | awk '{ print $4 }')" 
+  path="$(echo "$selected_device" | awk '{ print $1 }')"
+  uuid="$(echo "$selected_device" | awk '{ print $3 }')" 
+ 
+  echo "the path: $path"
+  if [ "$ret_value" = 1 ]; then
+    if [ "$selected_device" = "Refresh" ]; then
+      main
     else
-      if [ ! -d "$mount_path$3" ]; then
-        echo "creating mount point"
-        kdesu -c "mkdir -p $mount_path$4; mount $2 $mount_path$4"
-      else
-        kdesu -c "mount $2 $mount_path$4"
-      fi
+      exit 1;
     fi
   fi
-  
-  if [ "$?" -eq 1 ]; then
-    #TODO check this is reachable
-    zenity --error \
-      --text="$1ing failed, Aborting!" --timeout="$zenity_timeout"
-    echo "Aborted"
+
+  if [ "$selected_device" = "" ]; then
+    zenity --error --width=400 \
+    --text="No Device Selected, Quitting!"
     exit 1;
   fi
 
-  #TODO better feedback here, assumes no errors
-  zenity --info \
-    --text="$1ed!" --timeout="$zenity_timeout"
-  echo "Exit"
+  if [ "$mount_point" = "Unmounted" ]; then
+    confirm_gui "mount" "$path" "$uuid"
+    do_mount "mount" "$path"
+  else
+    confirm_gui "unmount" "$path" "$uuid"
+    do_mount "unmount" "$path"
+  fi
+
+  zenity --info --width=400 \
+    --text="$ret_value"
+
   exit 0;
 }
 
-function auto_mount () {
-  #1=uuid 2=fs
-  if grep -i '^UUID='$1 /etc/fstab; then
-    #TODO Add option to delete auto mount
-    zenity --error \
-      --width=600 \
-      --text="Mount Point $mount_path$1 Exists, Aborting!" --timeout="$zenity_timeout"
-    exit 1;
-  else
-    kdesu -c "echo -e \"\nUUID=$1 $mount_path$1 $2  defaults,nofail  0 0\" | tee -a \"/etc/fstab\""
-    
-    #TODO this might no longer be reachable
-    if [ "$?" = 1 ]; then
-      zenity --error \
-        --text="Writing to Fstab failed, Aborting!" --timeout="$zenity_timeout"
-      echo "Fail"
-      exit 1;
-    fi
-
-    if grep -i '^UUID='$1 /etc/fstab; then
-    zenity --info \
-      --width=600 \
-      --text="Mount Point $mount_path$1 will Auto Mount on Restart!" --timeout="$zenity_timeout"
-    fi
-    #TODO else!
-  fi
-}
-
-#run
-(
-  #sleep 1;
-  until [ -s "$external_drive_list" ]; do
-    #sleep 1;
-    if [ "$show_sd_card" -eq 0 ]; then
-      #RM "Removeable Drive" doens't show mmc cards
-      lsblk -ndo RM,NAME,SIZE | grep -i '^\s*1' > "$external_drive_list"
-    else
-      #HOTPLUG does show mmc drives
-      lsblk -ndo HOTPLUG,NAME,SIZE | grep -i '^\s*1' > "$external_drive_list"
-    fi
-
-    #TODO can get stuck in loop if dialog is closed
-  done
-  echo "# External Drive Found!"; sleep 2 #need the sleep for the re-check below
-  echo "100";
-) | zenity --progress \
-      --title="Please Insert External Drive" --text="Searching..." \
-      --width=300 \
-      --percentage=0 --pulsate --auto-close
-
-echo "$?"
-if [ "$?" = 1 ] ; then
-  echo "Aborted"
-  exit 1;
-fi
-
-#check again, list might not have been fully populated
-if [ "$show_sd_card" -eq 0 ]; then
-  lsblk -ndo RM,NAME,SIZE | grep -i '^\s*1' > "$external_drive_list"
-else
-  lsblk -ndo HOTPLUG,NAME,SIZE | grep -i '^\s*1' > "$external_drive_list"
-fi
-
-num_of_drives="$(wc -l < "$external_drive_list")" 
-
-  if [ $num_of_drives -gt 1 ]; then
-  selected_drive=$(zenity --list --title="Multiple Drives Found" \
-    --width=500 --height=500 --print-column=2 \
-    --separator='\t' --ok-label "Select Drive" \
-    --radiolist --column="Select" --column="Name" --column="Size" \
-    $(cat "$external_drive_list"))
-  if [ "$?" = 1 ] ; then
-    echo "Aborted"
-    exit 1;
-  fi
-else
-  #this is a bit poo but works
-  read -r line < "$external_drive_list"
-  column=($line)
-  selected_drive=${column[1]}
-fi
-
-#check we have selected a drive or die
-if [ ! -n "$selected_drive" ] ; then
-  echo "No Drive Selected"
-  exit 1;
-fi
-
-#get full info on the selected drives partitions
-lsblk -io NAME,SIZE,FSTYPE,UUID \
-  | grep -i '[\`|\|]\-'$selected_drive | sed 's/[\|\`][\-]//' > "$external_part_list"
-
-num_of_partitions="$(wc -l < "$external_part_list")" 
-
-if [ $num_of_partitions -gt 1 ]; then
-  selected_drive=$(zenity --list --title="Please Select a Partiton" \
-    --width=800 --height=200 --print-column=1 \
-    --separator='\t' --ok-label "Select" \
-    --column="Name" --column="Size" --column="Type" --column="UUID" \
-    $(cat "$external_part_list"))
-  
-  if [ "$?" = 1 ] ; then
-    echo "Aborted"
-    exit 1;
-  fi
-fi
-
-if [ $num_of_partitions -eq 1 ]; then
-  read -r line < "$external_part_list"
-  echo "$line"
-  column=($line) #0=NAME 1=SIZE 2=FSTYPE 4=UUID 3=MOUNTPOINT
-  selected_drive=${column[0]}
-fi
-
-if [ ! -n "$selected_drive" ] ; then
-  echo "No Drive Selected"
-  zenity --error \
-    --text="No Drive Selected, Aborting!" --timeout="$zenity_timeout"  
-  exit 1;
-fi
-
-
-#get full info on the drive/partiton
-lsblk -nlo NAME,SIZE,FSTYPE,UUID,LABEL,MOUNTPOINT \
-| grep -i '^'$selected_drive > "$external_drive_info"
-
-read -r line < "$external_drive_info"
-echo "$line"
-column=($line) #0=NAME 1=SIZE 2=FSTYPE 4=UUID 3=MOUNTPOINT
-
-if [ ! -n "${column[3]}" ]; then
-  echo "Error No UUID"
-  zenity --error \
-    --text="No UUID, Aborting!" --timeout="$zenity_timeout"
-  exit 1;
-fi
-
-mount_type="Mount"
-#TODO assumes the presence of a mount point that the drive is mounted. check.
-if [ -n "${column[5]}" ]; then
-  mount_type="Unmount"
-fi
-
-option=$(zenity --info --text="What would you like to do with /dev/${column[0]} \(${column[4]}\)?" --width=400 \
-    --extra-button "$mount_type" --extra-button "Auto Mount" \
-    --ok-label "Quit")
-  #1 means a button thats isn't "ok" was pressed
-  if [ "$?" = 1 ] ; then
-    if [ "$option" = "$mount_type" ]; then
-      do_mount "$mount_type" "/dev/${column[0]}" "${column[3]}" "${column[4]}" 
-      #TODO un-hardcode (3) /dev/ maybe check 0 has a value
-    else
-      if [ "$option" = "Auto Mount" ]; then
-        auto_mount "${column[3]}" "${column[2]}"
-      fi
-    fi
-  fi
-
-echo "script finshed"
-exit 0;
+main
