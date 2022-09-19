b6ebec90183f4ff4569ad1e4ad5df3abea939653
scawp <rob@scawp.com>
2022-09-19 10:53:01 +0200

    added curl install

--- curl_install.sh
+++ curl_install.sh
@@ -22,20 +22,22 @@ device_name="$(uname --nodename)"
 user="$(id -u deck)"
 
 if [ "$device_name" != "steamdeck" ] || [ "$user" != "1000" ]; then
-  echo -en "This code has been written specifically for the Steam Deck with user Deck \
+  zenity --question --width=400 \
+  --text="This code has been written specifically for the Steam Deck with user Deck \
   \nIt appears you are running on a different system/non-standard configuration. \
-  \nAre you sure you want to continue? (y/n) :"
-  read confirm
-  if [ "$confirm" != "y" ]; then
+  \nAre you sure you want to continue?"
+  if [ "$?" != 0 ]; then
+    #NOTE: This code will never be reached due to "set -e", the system will already exit for us but just incase keep this
     echo "bye then! xxx"
     exit 1;
   fi
 fi
 
-echo -en "Read $repo_url/README.md before proceeding. \
-\nDo you want to install the Auto-Mount Service? (y/n) :"
-read confirm
-if [ "$confirm" != "y" ]; then
+zenity --question --width=400 \
+  --text="Read $repo_url/README.md before proceeding. \
+\nDo you want to install the Auto-Mount Service?"
+if [ "$?" != 0 ]; then
+  #NOTE: This code will never be reached due to "set -e", the system will already exit for us but just incase keep this
   echo "bye then! xxx"
   exit 0;
 fi
@@ -43,13 +45,19 @@ fi
 echo "Making tmp folder $tmp_dir"
 mkdir -p "$tmp_dir"
 
+echo "Downloading Required Files"
+curl -o "$tmp_dir/automount.sh" "$repo_url/automount.sh"
+curl -o "$tmp_dir/template.service" "$repo_lib_dir/template.service"
+curl -o "$tmp_dir/99-external-drive-mount.rules" "$repo_lib_dir/99-external-drive-mount.rules"
+
 echo "Making script folder $script_install_dir"
 mkdir -p "$script_install_dir"
 
-echo "Downloading Required Files"
-curl -o "$tmp_dir/auto_mount.sh" "$repo_url/auto_mount.sh"
-curl -o "$tmp_dir/template.service" "$repo_lib_dir/template.service"
-curl -o "$tmp_dir/99-external-drive-mount.rules" "$repo_lib_dir/99-external-drive-mount.rules"
+echo "Copying $tmp_dir/automount.sh to $script_install_dir/automount.sh"
+sudo cp "$tmp_dir/automount.sh" "$script_install_dir/automount.sh"
+
+echo "Adding Execute and Removing Write Permissions"
+sudo chmod 555 $script_install_dir/automount.sh
 
 echo "Copying $tmp_dir/99-external-drive-mount.rules to $rules_install_dir/99-external-drive-mount.rules"
 sudo cp "$tmp_dir/99-external-drive-mount.rules" "$rules_install_dir/99-external-drive-mount.rules"
@@ -58,9 +66,6 @@ sed -e "s&\[AUTOMOUNTSCRIPT\]&$script_install_dir&g" "$tmp_dir/template.service"
 
 echo "Copying $tmp_dir/external-drive-mount@.service to $service_install_dir/external-drive-mount@.service"
 sudo cp "$tmp_dir/external-drive-mount@.service" "$service_install_dir/external-drive-mount@.service"
-
-echo "Adding Execute and Removing Write Permissions"
-chmod 555 $service_install_dir/automount.sh
 
 echo "Reloading Services"
 sudo udevadm control --reload
