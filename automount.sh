81a4f6794d829ce2a8264ad2b9799500630098d3
scawp <rob@scawp.com>
2022-09-29 21:53:31 +0200

    Should have tested this more....

--- automount.sh
+++ automount.sh
@@ -48,11 +48,11 @@ urlencode()
     url=$(urlencode "${mount_point}")
 
     # If Steam is running, notify it
-    if pgrep -x "steam" > /dev/null; then
+    #if pgrep -x "steam" > /dev/null; then
         # TODO use -ifrunning and check return value - if there was a steam process and it returns -1, the message wasn't sent
         # need to retry until either steam process is gone or -ifrunning returns 0, or timeout i guess
-        systemd-run -M 1000@ --user --collect --wait sh -c "./.steam/root/ubuntu12_32/steam steam://addlibraryfolder/${url@Q}"
-    fi
+    #    systemd-run -M 1000@ --user --collect --wait sh -c "./.steam/root/ubuntu12_32/steam steam://addlibraryfolder/${url@Q}"
+    #fi
   fi
 }
 
